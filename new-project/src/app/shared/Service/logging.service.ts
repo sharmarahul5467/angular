d03b37f14ba6eb2e755224db184e6681e5c2ddbc
Rahul Kumar Sharma <sharma.rahul5467@gmail.com>
2020-04-13 15:50:07 +0530

    setup learnng ng app
    setup learnng ng app

--- new-project/src/app/shared/Service/logging.service.ts
+++
@@ -1,5 +0,0 @@
-export class LoggingService {
-  logStatusChange(serverName: string = 'server', status: string){
-    console.log(`A ${serverName} Server status changed, new status is : ${status}`)
-  }
-}
